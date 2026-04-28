91c815a7accf63a5e169871232908fbdb038029a
Pavlo B <git@bpavlo.com>
2026-04-28 11:18:28 -0400

    chore: rm opencode, stylua

--- lua/settings.lua
+++ lua/settings.lua
@@ -21,8 +21,8 @@ vim.opt.clipboard:append("unnamedplus")
 vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
 
 vim.opt.guicursor = {
-  "n-v:block",
-  "i-c-ci-ve:ver25",
-  "r-cr:hor20",
-  "o:hor25",
+	"n-v:block",
+	"i-c-ci-ve:ver25",
+	"r-cr:hor20",
+	"o:hor25",
 }
